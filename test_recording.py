3315d63dba2714c9023dec4d56a0c98afa39b580
luis.sotochaves <luis.sotochaves@ucr.ac.cr>
2026-06-25 15:48:31 -0600

    codigo de prueba modificado

--- test_recording.py
+++ test_recording.py
@@ -1,5 +1,5 @@
 import sounddevice as sd
-import gpiod
+import RPi.GPIO as GPIO
 import time
 
 BUTTON_PIN = 17
@@ -7,21 +7,20 @@ SAMPLE_RATE = 8000
 MAX_BYTES = 480_000
 CHUNK = 1024
 
-chip = gpiod.Chip('gpiochip4')
-line = chip.get_line(BUTTON_PIN)
-line.request(consumer="ptt", type=gpiod.LINE_REQ_DIR_IN, flags=gpiod.LINE_REQ_FLAG_BIAS_PULL_UP)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(BUTTON_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 print("Listo. Mantenga presionado el botón para grabar...")
 
 try:
     while True:
-        if line.get_value() == 0:
+        if GPIO.input(BUTTON_PIN) == GPIO.LOW:
             print("Grabando...")
             buffer = bytearray()
 
             with sd.RawInputStream(samplerate=SAMPLE_RATE, channels=1, dtype='uint8',
                                    device='plughw:1,0', blocksize=CHUNK) as stream:
-                while line.get_value() == 0 and len(buffer) < MAX_BYTES:
+                while GPIO.input(BUTTON_PIN) == GPIO.LOW and len(buffer) < MAX_BYTES:
                     data, _ = stream.read(CHUNK)
                     buffer.extend(data)
 
@@ -35,4 +34,4 @@ try:
 except KeyboardInterrupt:
     print("\nSaliendo...")
 finally:
-    line.release()
+    GPIO.cleanup()
